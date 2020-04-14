43fea29c02b80ed3d71d1531bf801b76b7951564
boris42000 <62223325+boris42000@users.noreply.github.com>
2020-04-14 13:05:17 +0200

    Update lib.h
    All functions defined except root and exponent

--- src/lib.h
+++ src/lib.h
@@ -28,6 +28,46 @@ namespace MathFuncs
         static double Root(int ex, double a);
     };
 }
+double Add(double a, double b){
+    double result = a + b;
+    return result;
+}
+
+double Subtract(double a, double b){
+    double result = a - b;
+    return result;
+}
+
+double Multiply(double a, double b){
+    double result = a * b;
+    return result;
+}
+
+double Divide(double a, double b){
+    if (b == 0)
+    {
+         throw "Division by zero condition!";
+    }
+    double result = a / b;
+    return result;
+}
+
+double Factorial(double a){
+    double result = 1;
+
+    if (a < 0)
+    {
+        throw "Negative number condition!";
+    }
+
+
+    for(int i=1;i<=a;i++)
+    {
+      result = result * i;
+    }
+    return result;
+}
+
 
 
 #endif
