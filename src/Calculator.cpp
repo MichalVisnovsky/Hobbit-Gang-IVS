714000b7a0530daf4bf5521a019370e7d2daf4ee
boris42000 <62223325+boris42000@users.noreply.github.com>
2020-04-15 23:42:35 +0200

    Input of digits and point

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -1,11 +1,45 @@
-#include "Calculator.h"
+#include <Calculator.h>
 #include "./ui_Calculator.h"
+#include "lib.h"
+
+double firstNumber = 0;
+bool firstDigitIns = false;
+using namespace MathFuncs;
+
 
 Calculator::Calculator(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+
+    this->setWindowTitle("Calculator 2020");
+
+
+    ui->Display->setText(QString::number(firstNumber));
+    QPushButton *numButtons[10];
+    for(int i = 0 ;i < 10; i++)
+    {
+        QString butName = "NumButton" + QString::number(i);
+        numButtons[i] = Calculator::findChild<QPushButton *>(butName);
+        connect(numButtons[i], SIGNAL(released()), this, SLOT(digitPressed()));
+    }
+
+    connect(ui->AddButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->SubButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->DivButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->MulButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->ExpButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->RootButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->FactButton, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    
+    ui->AddButton->setCheckable(true);
+    ui->SubButton->setCheckable(true);
+    ui->DivButton->setCheckable(true);
+    ui->MulButton->setCheckable(true);
+    ui->ExpButton->setCheckable(true);
+    ui->RootButton->setCheckable(true);
+    ui->FactButton->setCheckable(true);
 }
 
 Calculator::~Calculator()
@@ -13,3 +47,31 @@ Calculator::~Calculator()
     delete ui;
 }
 
+
+void Calculator::digitPressed()
+{
+    QPushButton *button = (QPushButton *) sender();
+
+    double displayNumber;
+    QString newDisplay;
+
+    if ( ! firstDigitIns )
+    {
+        displayNumber = button->text().toDouble();
+        firstDigitIns = true;
+    }
+    else
+    {
+        displayNumber = ( ui->Display->text() + button->text()).toDouble();
+    }
+
+
+    newDisplay = QString::number(displayNumber,'g',16);
+    ui->Display->setText(newDisplay);
+}
+
+//point button
+void Calculator::on_pointButton_released()
+{
+    ui->Display->setText(ui->Display->text() + ".");
+}
